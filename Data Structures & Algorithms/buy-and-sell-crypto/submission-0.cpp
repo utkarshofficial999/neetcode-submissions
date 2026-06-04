afe8b91be64a2c708cd70b0c0bb243643f1ec2b6
utkrack <utkarshoffcial999@gmail.com>
2026-06-04 19:03:15 +0530

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int min_price=INT_MAX;
+        int max_profit=0;
+        for(int i=0;i<prices.size();i++){
+            if(prices[i]<min_price){
+                min_price=prices[i];
+            }
+            else{
+                int current_profit=prices[i]-min_price;
+                max_profit=max(max_profit,current_profit);
+            }
+        }
+        return max_profit;
+    }
+};
