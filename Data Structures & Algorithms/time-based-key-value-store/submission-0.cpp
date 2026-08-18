385d840c441b18775caa544595a1bdd96b7e673a
utkrack <utkarshoffcial999@gmail.com>
2026-08-19 00:47:16 +0530

    Add: time-based-key-value-store - submission-0

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-0.cpp
@@ -0,0 +1,33 @@
+class TimeMap {
+public:
+  unordered_map<string,vector<pair<int,string>>> mp;
+    TimeMap() {
+        ;
+    }
+    
+    void set(string key, string value, int timestamp) {
+        mp[key].push_back({timestamp,value});
+        
+    }
+    
+    string get(string key, int timestamp) {
+        if(mp.find(key)==mp.end()){
+            return "";}
+            vector<pair<int,string>> & arr=mp[key];
+            int left=0;
+            int right=arr.size()-1;
+          string  ans="";
+            while(left<=right){
+                int mid=(left+right)/2;
+                if(arr[mid].first<=timestamp){
+                    ans=arr[mid].second;
+                    left=mid+1;
+                }
+                else{
+                    right=mid-1;
+                }
+            }
+            return ans;
+        }
+    };
+
